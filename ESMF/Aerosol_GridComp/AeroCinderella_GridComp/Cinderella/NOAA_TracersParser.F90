0ae06f12f8f2ffb1470aed153668953174ad0ce3
William Jamieson <william.b.jamieson@nasa.gov>
2020-08-19 13:16:39 -0400

    Started tracer fallback parser

---
+++ ESMF/Aerosol_GridComp/AeroCinderella_GridComp/Cinderella/NOAA_TracersParser.F90
@@ -0,0 +1,10 @@
+#include "MAPL_Generic.h"
+
+module NOAA_TracersParser_mod
+    use ESMF
+    use MAPL
+    use gFTL_StringIntegerMap
+
+    implicit none
+contains
+end module NOAA_TracersParser_mod
