594f37de7082b6bc3ff5bf971d0daebfb5d0d5d5
William Jamieson <william.b.jamieson@nasa.gov>
2020-08-27 15:23:09 -0400

    Cleaned up tracer handlers

--- ESMF/Aerosol_GridComp/AeroCinderella_GridComp/Cinderella/ChemTracers.F90
+++
@@ -1,9 +0,0 @@
-module ChemTracers_mod
-    use ChemTracers_private_mod, only: read_tracer_config, TracerMap
-
-    implicit none
-    private
-
-    public read_tracer_config
-    public TracerMap
-end module ChemTracers_mod
