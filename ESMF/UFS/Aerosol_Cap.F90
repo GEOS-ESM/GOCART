d1d18c9a6d4b7258e2e9c1ea25201c794910d088
Raffaele Montuoro <raffaele.montuoro@noaa.gov>
2021-01-14 19:26:58 -0600

    Use updated MAPL Cap set_grid() API available since release 2.5.0.

--- ESMF/UFS/Aerosol_Cap.F90
+++ ESMF/UFS/Aerosol_Cap.F90
@@ -338,15 +338,8 @@ contains
 
     call this % cap_gc % set_services(_RC)
 
-    ! decorate grid object so it can be shared with MAPL
-    call ESMF_AttributeSet(grid, name='GRID_LM', value=nlev, rc=rc)
-    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__,  &
-      file=__FILE__)) &
-      return  ! bail out
-
     ! provide model grid to MAPL
-    call this % cap_gc % set_grid(grid, _RC)
+    call this % cap_gc % set_grid(grid, lm=nlev, _RC)
 
     ! provide model clock to MAPL
     call this % cap_gc % set_clock(clock, _RC)
