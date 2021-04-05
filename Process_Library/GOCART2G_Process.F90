ce3a1790e5c02d92b9e7a4a0a577309ff41c3dc5
Raffaele Montuoro <raffaele.montuoro@noaa.gov>
2021-04-05 21:33:31 +0000

    Update citation.

--- Process_Library/GOCART2G_Process.F90
+++ Process_Library/GOCART2G_Process.F90
@@ -272,7 +272,7 @@ CONTAINS
    real, parameter :: clay_thresh = 0.2    ! clay fraction above which the maximum flux ratio is returned
 
 ! !DESCRIPTION: Computes the vertical-to-horizontal dust flux ratio according to
-!               B.Marticorena, G.Bergametti, J.Geophys.Res., 100(D8), 16415–16430
+!               B.Marticorena, G.Bergametti, J.Geophys.Res., 100(D8), 16415–16430, 1995
 !               doi:10.1029/95JD00690
 !
 ! !REVISION HISTORY:
