ec7910a2463e6e795eafa62aee9ff269d05571b1
pchakraborty <pchakraborty@users.noreply.github.com>
2026-08-11 08:29:51 -0500

    Fix regression test setservices key and modernize expdir cleanup (#469)
    * Fix regression test key: setservice -> setservices
    
    parse_child in MAPL's ComponentSpecParser previously matched a
    truncated 'SetService' key due to a string-length bug (fixed in
    GEOS-ESM/MAPL#5307). These regression yamls were written to match
    that truncated key and need the trailing 's' restored now that the
    parser correctly requires the full 'setservices' key.
    
    * run_case.cmake: replaced execute_process(... rm -rf ...) with file(REMOVE_RECURSE ...)

--- ESMF/GOCART2G_GridComp/regression/run_case.cmake
+++ ESMF/GOCART2G_GridComp/regression/run_case.cmake
@@ -1,15 +1,5 @@
 include("${ESMA_REGRESSION_HELPERS}")
 
-function(copy_restarts root_dir expdir)
-  file(GLOB checkpoint_subdirs LIST_DIRECTORIES true ${root_dir}/checkpoints/*)
-  foreach(ckpt_dir IN LISTS checkpoint_subdirs)
-    get_filename_component(ckpt_name ${ckpt_dir} NAME)
-    if(IS_DIRECTORY ${ckpt_dir} AND NOT ckpt_name STREQUAL "last")
-      copy_directory(${ckpt_dir} ${expdir}/checkpoints/${ckpt_name})
-    endif()
-  endforeach()
-endfunction()
-
 function(run_case case_name regression_data_dir)
   string(RANDOM LENGTH 24 expdir)
   execute_process(
@@ -29,16 +19,9 @@ function(run_case case_name regression_data_dir)
   copy_restarts(${root_dir} ${expdir})
   link_directory(${regression_data_dir}/ExtData ${expdir}/ExtData)
   run_geos(${num_procs} ${case_name} ${expdir})
-
-  if(NOT EXISTS ${checkpoints_dir})
-    message(WARNING "Baseline directory [${checkpoints_dir}] not found. Skipping comparison.")
-    return()
-  endif()
   compare_results(${checkpoints_dir} ${expdir}/checkpoints/last)
 
-  # execute_process(
-  #   COMMAND ${CMAKE_COMMAND} -E rm -rf ${expdir}
-  # )
+  file(REMOVE_RECURSE ${expdir})
 endfunction()
 
 run_case(${TEST_CASE} ${REGRESSION_DATA_DIR})
