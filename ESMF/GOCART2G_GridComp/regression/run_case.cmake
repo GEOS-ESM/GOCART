243e2b8a27af585e152fcf3d73e385130313eafa
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2026-08-26 14:13:20 -0400

    GOCART2G regression: exclude coordinate variables

--- ESMF/GOCART2G_GridComp/regression/run_case.cmake
+++ ESMF/GOCART2G_GridComp/regression/run_case.cmake
@@ -21,7 +21,10 @@ function(run_case case_name regression_data_dir)
   run_geos(${num_procs} ${case_name} ${expdir})
 
   if(FORTRAN_COMPILER_ID STREQUAL "IntelLLVM") # only compare against IntelLLVM baselines
-    compare_results(${checkpoints_dir} ${expdir}/checkpoints/last NANS_ARE_EQUAL)
+    compare_results(${checkpoints_dir} ${expdir}/checkpoints/last
+      NANS_ARE_EQUAL
+      EXCLUDE_VARS lons lats corner_lons corner_lats
+    )
   endif()
 
   file(REMOVE_RECURSE ${expdir})
